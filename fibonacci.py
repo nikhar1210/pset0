85bdb1c5d06ce15e89f273b603742afb2e227cf3
Nikhar Shah <nikharshah@Nikhars-MacBook-Pro.local>
2021-01-18 02:03:42 -0500

    fibonacci last 8 function fixed

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 def last_8(some_int):
     """Return the last 8 digits of an int
@@ -13,9 +14,16 @@ def last_8(some_int):
 
     else:
 
-        return int(str(some_int)[len(str(some_int)) - 8 : len(str(some_int))])
+        return print(int(str(some_int)[len(str(some_int)) - 8: len(str(some_int))]))
 
 
 if __name__ == "__main__":
 
-    last_8()
+    parser = ArgumentParser(
+    description=__doc__, formatter_class=RawDescriptionHelpFormatter
+    )
+    parser.add_argument("-d", "--digits", type=int, default=12345, help="Number")
+
+    args = parser.parse_args()
+
+    last_8(args.digits)
